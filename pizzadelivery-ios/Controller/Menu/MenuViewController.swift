c5659878f57bac87301e0c43ec90969170f23832
Bruno Costa <bruncst@gmail.com>
2022-01-06 19:06:51 -0300

    add LogoView

--- pizzadelivery-ios/Controller/Menu/MenuViewController.swift
+++ pizzadelivery-ios/Controller/Menu/MenuViewController.swift
@@ -7,13 +7,17 @@
 
 import UIKit
 
-class MenuViewController: UIViewController, MenuBaseCoordinated, ViewConfiguration {
+class MenuViewController: UIViewController, MenuBaseCoordinated {
+    
+    // MARK: - Views
     
     var coordinator: MenuBaseCoordinator?
     
+    private let logoView = LogoView()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .white
+        setupViewConfiguration()
     }
     
     required init(coordinator: MenuBaseCoordinator) {
@@ -25,16 +29,27 @@ class MenuViewController: UIViewController, MenuBaseCoordinated, ViewConfigurati
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+}
+
+// MARK: - ViewConfiguration
+
+extension MenuViewController: ViewConfiguration {
     func setupConstraints() {
-        
+        logoView.translatesAutoresizingMaskIntoConstraints = false
+               
+        NSLayoutConstraint.activate([
+            logoView.topAnchor.constraint(equalTo: view.topAnchor, constant: 0),
+            logoView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0),
+            logoView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0),
+            logoView.heightAnchor.constraint(equalToConstant: 140)
+        ])
     }
     
     func buildViewHierarchy() {
-        
+        view.addSubview(logoView)
     }
     
     func configureViews() {
-        
+        view.backgroundColor = .white
     }
 }
---
+++ pizzadelivery-ios/Views/Custom Views/LogoView.swift
@@ -0,0 +1,79 @@
+//
+//  LogoView.swift
+//  pizzadelivery-ios
+//
+//  Created by Bruno Costa on 06/01/22.
+//
+
+import UIKit
+
+class LogoView: UIView {
+    
+    // MARK: - Views
+    
+    private let backgroundImage: UIImageView = {
+        let backgroundImage = UIImageView(image: UIImage(named: "backgroundImage"))
+        backgroundImage.contentMode = .scaleAspectFill
+        backgroundImage.clipsToBounds = true
+        return backgroundImage
+    }()
+    
+    private let logoImage: UIImageView = {
+        let logoImage = UIImageView(image: UIImage(named: "logoImage"))
+        logoImage.contentMode = .scaleAspectFill
+        logoImage.clipsToBounds = true
+        return logoImage
+    }()
+    
+    // MARK: - Inicialization
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViewConfiguration()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemeted")
+    }
+    
+    // MARK: - Setup Constraints
+    
+    private func setupHeaderImageConstraints() {
+        backgroundImage.translatesAutoresizingMaskIntoConstraints = false
+            
+        NSLayoutConstraint.activate([
+            backgroundImage.topAnchor.constraint(equalTo: self.topAnchor, constant: 0),
+            backgroundImage.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 0),
+            backgroundImage.rightAnchor.constraint(equalTo: self.rightAnchor, constant: 0),
+            backgroundImage.heightAnchor.constraint(equalToConstant: 150)
+        ])
+    }
+    private func setupLogoConstraints() {
+        logoImage.translatesAutoresizingMaskIntoConstraints = false
+            
+        NSLayoutConstraint.activate([
+            logoImage.centerXAnchor.constraint(equalTo: backgroundImage.centerXAnchor),
+            logoImage.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: 25),
+            logoImage.heightAnchor.constraint(equalToConstant: 80),
+            logoImage.widthAnchor.constraint(equalToConstant: 80)
+        ])
+    }
+}
+
+// MARK: - ViewConfiguration
+
+extension LogoView: ViewConfiguration {
+    func setupConstraints() {
+        setupHeaderImageConstraints()
+        setupLogoConstraints()
+    }
+    
+    func buildViewHierarchy() {
+        addSubview(backgroundImage)
+        addSubview(logoImage)
+    }
+    
+    func configureViews() {
+        backgroundColor = . white
+    }
+}
