50108c600354a1c32851bed4406acce50fb7435d
Bruno Costa <bruncst@gmail.com>
2022-02-28 10:25:24 -0300

    change font size labels

--- pizzadelivery-ios/Views/Custom Cells/MenuTableViewCell/MenuTableViewCell.swift
+++ pizzadelivery-ios/Views/Custom Cells/MenuTableViewCell/MenuTableViewCell.swift
@@ -16,8 +16,8 @@ class MenuTableViewCell: UITableViewCell {
         dishImage.clipsToBounds = true
         return dishImage
     }()
-    let titleLabel: UILabel = MyLabel(font: UIFont(name: "avenir", size: 16)!, textColor: .black, numberOfLines: 1)
-    let descriptionLabel: UILabel = MyLabel(font: UIFont(name: "avenir", size: 14)!, textColor: .gray, numberOfLines: 2)
+    let titleLabel: UILabel = MyLabel(font: UIFont(name: "avenir-heavy", size: 14)!, textColor: .systemGray, numberOfLines: 1)
+    let descriptionLabel: UILabel = MyLabel(font: UIFont(name: "avenir", size: 15)!, textColor: .gray, numberOfLines: 2)
     let priceLabel: UILabel = MyLabel(font: UIFont(name: "avenir", size: 16)!, textColor: .black, numberOfLines: 0)
     
     // MARK: - Initialization
